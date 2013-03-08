488507729606f4fe249757caf347da6c11c3eba4
alexis <serres.alexis@gmail.com>
2013-03-08 15:57:14 +0100

    initial commit

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ exports.values = {
 	version: '0.0.1',
 	server : {
 		production : {
-			real_time_server : {port: 1337, host: 'localhost'}
+			real_time_server : {port: 37203, host: 'localhost'}
 		}
 	}
 }
