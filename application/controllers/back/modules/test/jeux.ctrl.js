22e0752120c2341950be0e942d5be87b1dc34ffc
LaPatatePourrie <alexiserres@hotmail.com>
2013-03-14 10:38:58 +0100

    new file:   application/controllers/back/modules/emprunts/categories.ctrl.js 	new file:   application/controllers/back/modules/emprunts/emprunts.ctrl.js 	new file:   application/controllers/back/modules/emprunts/oeuvres.ctrl.js 	new file:   application/controllers/back/modules/test/category.ctrl.js 	new file:   application/controllers/back/modules/test/jeux.ctrl.js 	new file:   application/controllers/back/modules/test/users.ctrl.js 	new file:   application/views/back/menu.jade 	new file:   public/img/icon/arobase.png 	new file:   public/img/icon/arrow_selection.png 	new file:   public/img/icon/arrow_selection_hover.png 	new file:   public/img/icon/calendar.png 	new file:   public/img/icon/locked.png 	new file:   public/img/icon/package.png

---
+++ application/controllers/back/modules/test/jeux.ctrl.js
@@ -0,0 +1,122 @@
+this.statut = 4;
+
+exports.load = function (param, callback) {
+	var module = {
+		name 			: 'jeux',
+		title 			: 'Jeux',
+		table 			: 'jeux',
+		elempp			: 100,
+		orderby 		: {
+				field 		: 'titre',
+				asc			: -1
+		},
+		
+		restrict : {
+			list	: 5
+		},
+		
+		actions			: {
+				add			: true,
+				mod			: true,
+				del			: true
+		},
+		
+		
+		fields 		 	: {
+				order		: ['nom', 'titre', 'category', 'description', 'min', 'max', 'color'],
+				all			: {
+						nom		: {
+								restrict		: 5,
+								label 		: {
+										list		: 'Nom',
+										form		: 'Nom'
+								},
+								param		: {
+										type 		: 'input'
+								},
+								display 	: {
+										list 			: true,
+										form			: true,
+										maxLength		: 3
+								}
+						},
+						titre		: {
+								label 		: {
+										list			: 'Titre',
+										form			: 'Titre'
+								},
+								param		: {
+										type 		: 'input'
+								}
+						},
+						category	: {
+								label 		: {
+										list			: 'Cat&eacute;gorie'
+								},
+								param		: {
+										type 		: 'input',
+										autocomplete 	: true,
+										value		: {
+												embedded		: false,
+												allowOther		: true,
+												fixed			: false,
+												dynamic 			: {
+														table 		: 'categories',
+														fields		: 'titre',
+														order		: 'titre',
+														separator	: ','
+												}
+										}
+								}
+						},
+						min		: {
+								parent		: 'nbPlayer',
+								label 		: {
+										list			: 'Min player',
+										form			: 'Min player'
+								},
+								param		: {
+										type 		: 'input'
+								}
+						},
+						max		: {
+								parent		: 'nbPlayer',
+								label 		: {
+										list			: 'Max Player',
+										form			: 'Max Player'
+								},
+								param		: {
+										type 		: 'input'
+								}
+						},
+						description		: {
+								param		: {
+										type 		: 'textarea',
+										widget		: {
+											wysiwyg		: 'ckeditor',
+											options		: {}
+										}
+								},
+								label 		: {
+										list			: 'Description',
+										form			: 'Description'
+								}
+						},
+						color		: {
+								label 		: {
+										list			: 'Couleur',
+										form			: 'Couleur'
+								},
+								param		: {
+										type 		: 'input',
+										widget		: {
+											colorpicker		: {}
+										}
+								}
+						}
+				}
+		}
+	};
+	
+	return module;
+};
