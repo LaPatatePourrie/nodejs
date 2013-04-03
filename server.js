90ec1d8b416b1c699644527c1cbf7cd54c2ba168
LaPatatePourrie <alexiserres@hotmail.com>
2013-04-03 12:04:10 +0200

    update node packages: express, socket, io.authorization...

--- server.js
+++ server.js
@@ -6,13 +6,44 @@ var loader = require('./application/library/Loader.js').loadGlobals();
 // Application
 var config = require('./application/config/config').values;
 var app = require('./application/config/app').load(config)
-var io = require('socket.io').listen(app);
+var server = http.createServer(app);
 
 
+// Socket IO
+var io = require('socket.io').listen(server);
+
+io.set('authorization', function (data, accept) {
+	if (!data.headers.cookie) {
+		return accept('Session cookie required.', false);
+	} 
+
+	data.cookie = require('express/node_modules/cookie').parse(data.headers.cookie);
+	data.cookie = require('express/node_modules/connect/lib/utils').parseSignedCookies(data.cookie, 'secret');
+ 	
+	/* NOTE: save ourselves a copy of the sessionID. */
+	data.sessionID = data.cookie['express.sid'];
+	/* NOTE: get the associated session for this ID. If it doesn't exist,
+	* then bail. */
+	app.sessionStore.get(data.sessionID, function(err, session){
+		if (err)  			return accept('Error in session store.', false);
+		else if (!session)  return accept('Session not found.', false);
+
+		console.log('success !');
+		console.log(session);
+		// success! we're authenticated with a known session.
+		data.session = session;
+		return accept(null, true);
+	});
+});
+
+
+
+// Listen
 var port = 1337;
-app.listen(port);
+server.listen(port);
 
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+console.log("Express server listening on port %d", server.address().port);
+
 
 
 // Routes
@@ -20,7 +51,7 @@ require('./application/config/router').load(app, io);
 
 
 process.on('SIGINT', function () {
-	app.close();
+	server.close();
 	console.log();
 	console.log('Shutting down server..');
 	process.exit(0);
